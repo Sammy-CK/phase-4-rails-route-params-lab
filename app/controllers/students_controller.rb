651e3b89e189108c654669c9a5c8bee20571e700
Sammy-CK <sammycherono2696@gmail.com>
2023-03-14 12:47:25 +0300

    Completed tasks

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def info
+    student_info = Student.find(params[:id])
+    render json: student_info
+  end
+
 end
